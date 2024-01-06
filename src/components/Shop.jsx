4d444bd7a9cdf8d691d37f67c514343f50e57529
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-06 12:03:17 +0530

    Winter section start

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -12,14 +12,14 @@ const Shop = () => {
                 <div className="cards">
                     <div className="cards-wrapper">
                         <Card title="Jar" img="img1.jpg" />
-                        <Card title="Jar" img="img2.jpg" />
-                        <Card title="Jar" img="img3.jpg" />
-                        <Card title="Jar" img="img4.jpg" className="center" />
+                        <Card title="show Case" img="img2.jpg" />
+                        <Card title="vase " img="img3.jpg" />
+                        <Card title="table Essence" img="img4.jpg" className="center" />
                     </div>
                     <div className="cards-wrapper">
-                        <Card title="Jar" img="img1.jpg" />
-                        <Card title="Jar" img="img2.jpg" />
-                        <Card title="Jar" img="img3.jpg" />
+                        <Card title="Lighting " img="img1.jpg" />
+                        <Card title="Wall Decor" img="img2.jpg" />
+                        <Card title="Antiques" img="img3.jpg" />
                         <Card title="Jar" img="img4.jpg" className="center" />
                     </div>
                 </div>
