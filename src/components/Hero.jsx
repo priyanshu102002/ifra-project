fd8a82bbb5376b4d2d945a219cfb611cfa8d1f51
priyanshukr449 <priyanshukr449@gmail.com>
2024-01-06 10:11:02 +0530

    hero section done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./Hero.css";
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            <div className="hero-wrapper">
+                <h3>
+                    "Where History Meets Home: Discover Timeless Treasures."
+                </h3>
+                <h5>Antiques Unearthed, Stories Rediscovered."</h5>
+                <button>Explore Now</button>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
